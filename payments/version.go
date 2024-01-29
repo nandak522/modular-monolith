aefedeb6143f710ccc5bc64dffb05c4624454fb9
Nanda Kishore <nandakishore.b@razorpay.com>
2024-01-29 11:15:56 +0530

    Add Github Workflows

---
+++ payments/version.go
@@ -0,0 +1,9 @@
+package main
+
+// VERSION denotes package version number of payments
+// Follow https://semver.org/
+var VERSION = [...]string{
+	"0", // MAJOR,
+	"1", // MINOR,
+	"0", // PATCH,
+}
